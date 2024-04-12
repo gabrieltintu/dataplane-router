99f7c74c6dceab544fd2cff34dadf10bb6c8a299
Gabriel Tintu <gabriel.tintu@yahoo.com>
2024-04-12 16:42:36 +0000

    final solution/ readme included

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-Repository for the first homework of the Communication Networks class. In this homework the students will implement the dataplane of a router.
+### TINTU Gabriel-Claudiu 323CAb - 2023-2024
+
+### PCOM - first assignment
+# Dataplane-router
+
+#### Solved the IPv4 and ICMP tasks
+
+Start by parsing the routing and ARP table and sort the routing one in a
+descending order.
+
+In a loop, process every packet and check if it is an IPv4 and if the router
+got an "Echo request".
+
+## IPv4
+
+Check the integrity of the IP header & the ttl and drop the packet if those
+fields are not ok.
+Use the function *get_best_route* to binary search (through the sorted routing
+table) the IP address of the destination in order to determine the next hop
+address. If there is no route drop the packet.
+Recompute the checksum using the old ttl, new_ttl and old checksum.
+Rewrite the ethernet header addresses so that the sent frame is correct.
+
+## ICMP
+
+3 types of ICMP messages can be sent:
+1. Echo reply - sent when the router gets an echo request;
+2. Destination unreachable - sent when there is sno route to the destination;
+3. Time exceeded - sent if the packet is dropped due to the expiring of ttl;
+
+For the first type check if the router got an "Echo request". If so call
+*send_echo_reply*. The function properly changes the fields in the ip and
+icmp headers such as: swapping the destination address, recomputing the
+checksums and set the type for an "Echo reply".
+
+For the destination unreachable and time exceeded call *send_icmp*. In the
+function, create a new buffer that is going to be sent. The new buffer will
+contain a new __ether header__, __ip header__, __icmp header__, the 
+__ip header__ from the dropped packet, and 8 bytes from it.
+Set the addresses from the ETH header, build the new IP header accordingly
+and set the fields for the ICMP header corresponding to the message that
+needs to be sent (TIME_EXCEEDED or DST_UNREACHABLE). Put the headers in the
+new buffer and send it.
--- router.c
+++ router.c
@@ -4,7 +4,7 @@
 #include "lib.h"
 #include "protocols.h"
 
-#define HOST_UNREACHABLE 0
+#define DST_UNREACHABLE 0
 #define TIME_EXCEEDED 1
 
 /* Routing table */
@@ -16,79 +16,78 @@ struct arp_table_entry *arp_table;
 int arp_table_len;
 
 /*
- Returns a pointer (eg. &rtable[i]) to the best matching route, or NULL if there
+ Binary search through the sorted routing table.
+ Returns a pointer to the best matching route, or NULL if there
  is no matching route.
 */
-struct route_table_entry *get_best_route(uint32_t ip_dest) {
-
+struct route_table_entry *get_best_route(uint32_t ip_dest)
+{
 	int low = 0;
-    int high = rtable_len - 1;
-    struct route_table_entry *best_match = NULL;
+	int high = rtable_len - 1;
+	struct route_table_entry *best_match = NULL;
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
+	while (low <= high) {
+		int mid = low + (high - low) / 2;
 
-        // Check if the current entry matches the destination IP address
-        if ((ip_dest & rtable[mid].mask) == rtable[mid].prefix) {
-            // Update the best match if the current entry has a longer prefix
-            best_match = &rtable[mid];
-            // Continue searching for a longer match
-            high = mid - 1;
-        } else if ((ip_dest & rtable[mid].mask) > rtable[mid].prefix) {
-            // If the prefix of the current entry is less than the masked destination IP
-            // Move to the lower half of the table
-            high = mid - 1;
-        } else {
-            // If the prefix of the current entry is greater than the masked destination IP
-            // Move to the upper half of the table
-            low = mid + 1;
-        }
-    }
-
-    return best_match;
-}
-
-int compare_masks_desc(const void *a, const void *b) {
-    struct route_table_entry *r_table_entry_1 = (struct route_table_entry *)a;
-    struct route_table_entry *r_table_entry_2 = (struct route_table_entry *)b;
-
-    // Comparați măștile în ordine descrescătoare
-	if (r_table_entry_1->prefix > r_table_entry_2->prefix) {
-		return -1;
-	} else if (r_table_entry_1->prefix < r_table_entry_2->prefix) {
-		return 1;
-	} else {
-		if (r_table_entry_1->mask > r_table_entry_2->mask) {
-			return -1;
-		} else if (r_table_entry_1->mask < r_table_entry_2->mask) {
-			return 1;
+		if ((ip_dest & rtable[mid].mask) == rtable[mid].prefix) {
+			best_match = &rtable[mid];
+			// Move to the left side to search for a better match
+			high = mid - 1;
+		} else if ((ip_dest & rtable[mid].mask) > rtable[mid].prefix) {
+			// Move to the left side
+			high = mid - 1;
 		} else {
-			return 0;
-    	}
+			// Move to the right side
+			low = mid + 1;
+		}
 	}
+
+	return best_match;
 }
 
-void sort_rtable(void) {
-    qsort(rtable, rtable_len, sizeof(struct route_table_entry), compare_masks_desc);
+int compare_func(const void *a, const void *b)
+{
+	struct route_table_entry *r_table_entry_1 = (struct route_table_entry *)a;
+	struct route_table_entry *r_table_entry_2 = (struct route_table_entry *)b;
+
+	// Sort by prefix and mask in a descending order
+	if (r_table_entry_1->prefix > r_table_entry_2->prefix)
+		return -1;
+	else if (r_table_entry_1->prefix < r_table_entry_2->prefix)
+		return 1;
+
+	if (r_table_entry_1->mask > r_table_entry_2->mask)
+		return -1;
+	else if (r_table_entry_1->mask < r_table_entry_2->mask)
+		return 1;
+	else
+		return 0;
 }
 
-struct arp_table_entry *get_arp_entry(uint32_t given_ip) {
-	/* TODO 2.4: Iterate through the MAC table and search for an entry
-	 * that matches given_ip. */
+void sort_rtable(void)
+{
+	qsort(rtable, rtable_len, sizeof(struct route_table_entry), compare_func);
+}
 
-	/* We can iterate thrpigh the mac_table for (int i = 0; i <
-	 * mac_table_len; i++) */
-
+struct arp_table_entry *get_arp_entry(uint32_t given_ip)
+{
+	// Search for an entry that matches given_ip
 	for (int i = 0; i < arp_table_len; i++)
 		if (arp_table[i].ip == given_ip)
 			return &arp_table[i];
-	
+
 	return NULL;
 }
 
-void send_icmp(struct ether_header *eth_hdr, struct iphdr *ip_hdr, char *payload, int type, int interface) {
+/*
+ Sends an ICMP message when destination is unreachable or time is exceeded.
+*/
+void send_icmp(struct ether_header *eth_hdr, struct iphdr *ip_hdr, char *payload, int type, int interface)
+{
+	// Allocate the new buffer that will be sent
 	size_t new_buf_size = sizeof(struct ether_header) + 2 * sizeof(struct iphdr) + sizeof(struct icmphdr) + 8;
 	char *new_buf = malloc(new_buf_size);
+	DIE(!new_buf, "memory");
 
 	memcpy(eth_hdr->ether_shost, eth_hdr->ether_dhost, 6);
 	get_interface_mac(interface, eth_hdr->ether_shost);
@@ -99,14 +98,17 @@ void send_icmp(struct ether_header *eth_hdr, struct iphdr *ip_hdr, char *payload
 	curr += sizeof(struct ether_header);
 
 	struct icmphdr *icmp_hdr = malloc(sizeof(struct icmphdr));
+	DIE(!icmp_hdr, "memory");
 
 	struct iphdr *ip_header = malloc(sizeof(struct iphdr));
+	DIE(!ip_header, "memory");
 
+	// Set the fields for the new IP header
 	ip_header->tos = 0;
-	ip_header->frag_off = htons(0);
+	ip_header->frag_off = 0;
 	ip_header->version = 4;
 	ip_header->ihl = 5;
-	ip_header->id = htons(1);
+	ip_header->id = 1;
 	ip_header->ttl = 20;
 	ip_header->protocol = 1;
 	ip_header->tot_len = htons(new_buf_size - curr);
@@ -118,7 +120,8 @@ void send_icmp(struct ether_header *eth_hdr, struct iphdr *ip_hdr, char *payload
 	memcpy(new_buf + curr, ip_header, sizeof(struct iphdr));
 	curr += sizeof(struct iphdr);
 
-	if (type == HOST_UNREACHABLE) {
+	// Set the ICMP header fields accordingly
+	if (type == DST_UNREACHABLE) {
 		icmp_hdr->type = 3;
 		icmp_hdr->code = 0;
 	} else if (type == TIME_EXCEEDED) {
@@ -136,15 +139,24 @@ void send_icmp(struct ether_header *eth_hdr, struct iphdr *ip_hdr, char *payload
 
 	memcpy(new_buf + curr, payload, 8);
 
+	// Send the new buffer
 	send_to_link(interface, new_buf, new_buf_size);
 
-
+	free(new_buf);
+	free(icmp_hdr);
+	free(ip_header);
 }
 
-void send_echo_reply(char *buf, struct ether_header *eth_hdr, struct iphdr *ip_hdr, struct icmphdr *icmp_hdr, int len, int interface) {
+/*
+ Sends an echo reply when the router receives an ICMP message of type
+ echo request.
+*/
+void send_echo_reply(char *buf, struct ether_header *eth_hdr, struct iphdr *ip_hdr, struct icmphdr *icmp_hdr, int len, int interface)
+{
 	memcpy(eth_hdr->ether_dhost, eth_hdr->ether_shost, 6);
 	get_interface_mac(interface, eth_hdr->ether_shost);
 
+	// Swap the source and destination addresses
 	uint32_t tmp;
 	tmp = ip_hdr->saddr;
 	ip_hdr->saddr = ip_hdr->daddr;
@@ -152,13 +164,12 @@ void send_echo_reply(char *buf, struct ether_header *eth_hdr, struct iphdr *ip_h
 	ip_hdr->check = 0;
 	ip_hdr->check = htons(checksum((uint16_t *)ip_hdr, sizeof(struct iphdr)));
 
-
 	icmp_hdr->type = 0;
 	icmp_hdr->code = 0;
 	icmp_hdr->checksum = htons(checksum((uint16_t *)icmp_hdr, len - (sizeof(struct ether_header) + sizeof(struct iphdr))));
 
+	// Send the buffer with the changed fields
 	send_to_link(interface, buf, len);
-
 }
 
 int main(int argc, char *argv[])
@@ -169,100 +180,98 @@ int main(int argc, char *argv[])
 	init(argc - 2, argv + 2);
 
 	rtable = malloc(sizeof(struct route_table_entry) * 1000000);
-	/* DIE is a macro for sanity checks */
-	DIE(rtable == NULL, "memory");
+	DIE(!rtable, "memory");
 
 	arp_table = malloc(sizeof(struct  arp_table_entry) * 100);
-	DIE(arp_table == NULL, "memory");
-	
-	/* Read the static routing table and the MAC table */
+	DIE(!arp_table, "memory");
+
+	/* Read the static routing table and the ARP table */
 	rtable_len = read_rtable(argv[1], rtable);
 
 	arp_table_len = parse_arp_table("arp_table.txt", arp_table);
+
 	sort_rtable();
 
 	while (1) {
-
 		int interface;
 		size_t len;
 
 		interface = recv_from_any_link(buf, &len);
 		DIE(interface < 0, "recv_from_any_links");
 
-		struct ether_header *eth_hdr = (struct ether_header *) buf;
-		/* Note that packets received are in network order,
-		any header field which has more than 1 byte will need to be conerted to
-		host order. For example, ntohs(eth_hdr->ether_type). The oposite is needed when
-		sending a packet on the link, */
+		struct ether_header *eth_hdr = (struct ether_header *)buf;
 
 		/* Check if we got an IPv4 packet */
 		if (ntohs(eth_hdr->ether_type) != ETHERTYPE_IP) {
 			printf("Ignored non-IPv4 packet\n");
 			continue;
 		}
-		
+
 		struct iphdr *ip_hdr = (struct iphdr *)(buf + sizeof(struct ether_header));
 
-		/* TODO 2.1: Check the ip_hdr integrity using ip_checksum((uint16_t *)ip_hdr, sizeof(struct iphdr)) */
-		
+		// Send echo reply
 		if (ip_hdr->protocol == 1 && ip_hdr->daddr == inet_addr(get_interface_ip(interface))) {
 			struct icmphdr *icmp_hdr = (struct icmphdr *)(buf + sizeof(struct ether_header) + sizeof(struct iphdr));
+			DIE(!icmp_hdr, "memory");
+
 			if (icmp_hdr->type == 8) {
 				send_echo_reply(buf, eth_hdr, ip_hdr, icmp_hdr, len, interface);
 				continue;
 			}
 		}
 
-
+		// Check the ip_hdr integrity with checksum and drop if not ok
 		int checksum_ret = ip_hdr->check;
 		ip_hdr->check = 0;
 
 		if (checksum((uint16_t *)ip_hdr, sizeof(struct iphdr)) != ntohs(checksum_ret))
 			continue;
-		
+
 		ip_hdr->check = checksum_ret;
 
+		// Find the best route
+		// Send an ICMP message and drop if destination is unreachable
 
-		/* TODO 2.2: Call get_best_route to find the most specific route, continue; (drop) if null */
+		size_t size = sizeof(struct ether_header) + sizeof(struct iphdr);
 
 		struct route_table_entry *best_route = get_best_route(ip_hdr->daddr);
-		if (best_route == NULL) {
-			send_icmp(eth_hdr, ip_hdr, buf + sizeof(struct ether_header) + sizeof(struct iphdr), HOST_UNREACHABLE, interface);
+		if (!best_route) {
+			send_icmp(eth_hdr, ip_hdr, buf + size, DST_UNREACHABLE, interface);
 			continue;
 		}
 
-
-		/* TODO 2.3: Check TTL >= 1. Update TLL. Update checksum  */
+		// Check time_to_live to avoid looping
+		// Send ICMP and drop if time is exceeded
 		int ttl = ip_hdr->ttl;
+
 		if (ttl > 1) {
 			ip_hdr->ttl--;
 		} else {
-			send_icmp(eth_hdr, ip_hdr, buf + sizeof(struct ether_header) + sizeof(struct iphdr), TIME_EXCEEDED, interface);
+			send_icmp(eth_hdr, ip_hdr, buf + size, TIME_EXCEEDED, interface);
 			continue;
 		}
 
-		int new_checksum = ~(~ip_hdr->check +  ~((uint16_t)ttl) + (uint16_t)ip_hdr->ttl) - 1;
+		// Update the checksum after decreasing ttl
+		int new_checksum = ~(~ip_hdr->check + ~((uint16_t)ttl) + (uint16_t)ip_hdr->ttl) - 1;
 		ip_hdr->check = new_checksum;
-		
-		/* TODO 2.4: Update the ethernet addresses. Use get_arp_entry to find the destination MAC
-		 * address. Use get_interface_mac(m.interface, uint8_t *mac) to
-		 * find the mac address of our interface. */
-		
-		
+
+		// Update the ethernet addresses
 		uint8_t mac[6];
-		
+
 		get_interface_mac(best_route->interface, mac);
 		struct arp_table_entry *ret = get_arp_entry(best_route->next_hop);
-		if (ret == NULL) {
-			printf("No matching MAC entry found for destination IP. Packet dropped.\n");
+		if (!ret) {
+			printf("No matching ARP entry found for destination IP\n");
 			continue;
 		}
-		
 
 		memcpy(eth_hdr->ether_dhost, ret->mac, 6);
 		memcpy(eth_hdr->ether_shost, mac, 6);
-		
+
 		send_to_link(best_route->interface, buf, len);
 	}
+
+	free(rtable);
+	free(arp_table);
 }
 
